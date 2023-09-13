4d0cf10883cb2f049841d9e11d9036b13d8c55ab
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-12 20:00:10 -0700

    lab done

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+        def bark(self):
+            print("Woof!")
 
-    #Instance method definition
-    pass
+        def sit(self):
+            print("The dog is sitting.")
+#Instance method definition
+fido = Dog()
+fido.bark()
+
+snoppy = Dog()
+snoppy.bark()
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,9 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
-    pass
